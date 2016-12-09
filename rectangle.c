04bad57dc83d917b7a1fb8d6261c0c31bccec6e2
saranyakabaleeswaran <saranyakabaleeswaran@gmail.com>
2016-12-09 11:42:16 +0530

    Create rectangle.c

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int v1,v2,len,bre;
+  printf("Enter the x-coordinate of the left bottom corner\n");
+  scanf("%d",&v1);
+  printf("Enter the y-coordinate of the left bottom corner\n");
+  scanf("%d",&v2);
+  printf("Enter the length of the rectangle\n");
+  scanf("%d",%len);
+  printf("Enter the breadth of the rectangle\n");
+  scanf("%d",&bre);
