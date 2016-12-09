ff2f68182235423cb83d9f820c30c13165954b2d
saranyakabaleeswaran <saranyakabaleeswaran@gmail.com>
2016-12-09 11:52:15 +0530

    Create collectflags1.c

---
+++ collectflags1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  int main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+     printf("%d is an even number\n",n);
+  }
+  else
+  {
+    printf("%d is an odd number\n",n);
+  }
+  return 0;
+}
