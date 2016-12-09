75b1f4915410133bd030e9aac42bd5770e442e64
saranyakabaleeswaran <saranyakabaleeswaran@gmail.com>
2016-12-09 11:11:55 +0530

    Rename welcome.c to welcomes.c

---
+++ welcomes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char name[50];
+  printf("Enter your name :\n");
+  scanf("%s",name);
+ printf("Hello %s ! Welcome to Disneyland!!!",name);
+  return 0;
+}
