b73c1f443189a7b4581fe10d262d223e0a5cbc43
saranya k <saranyakabaleeswaran@gmail.com>
2016-12-19 13:44:49 +0530

    Create compare3.c

---
+++ compare3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+printf("a is greater");
+elseif(b>c)
+printf("b is greater");
+else
+printf("c is greater");
+return 0;
+}
