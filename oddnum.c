c9aff664f02bb8987627a4e448895b24065113b5
saranya k <saranyakabaleeswaran@gmail.com>
2016-12-22 13:45:52 +0530

    Update oddnum.c

--- oddnum.c
+++ oddnum.c
@@ -5,10 +5,10 @@ int main()
     int i, n;
     
    
-    printf("Print odd numbers till: ");
+   
     scanf("%d", &n);
 
-    printf("All odd numbers from 1 to %d are: \n", n);
+    printf(" odd numbers from 1 to %d are: \n", n);
 
     
     for(i=1; i<=n; i++)
