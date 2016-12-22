db6b2f329651f456b064bc71957dbe1140e3fdc2
saranya k <saranyakabaleeswaran@gmail.com>
2016-12-22 13:51:31 +0530

    Create evennum.c

---
+++ evennum.c
@@ -0,0 +1,23 @@
+int main()
+{
+    int i, n;
+   
+    
+ 
+    scanf("%d", &n);
+ 
+    printf("All even numbers from 1 to %d are: \n", n);
+ 
+  
+    for(i=1; i<=n; i++)
+    {
+       
+        if(i%2==0)
+        {
+            printf("%d\n", i);
+        }
+    }
+ 
+    return 0;
+}
+
