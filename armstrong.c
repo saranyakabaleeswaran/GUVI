0a28c64498d66a74f84aa747b196dea6b4871fe4
saranya k <saranyakabaleeswaran@gmail.com>
2016-12-22 14:14:29 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int num, a, r, result = 0;
+
+  
+    scanf("%d", &num);
+
+    a = num;
+
+    while (a != 0)
+    {
+        r = a%10;
+        result += r*r*r;
+      a /= 10;
+    }
+
+    if(result == num)
+        printf( "Armstrong number",number);
+    else
+        printf("s not an Armstrong number",number);
+
+    return 0;
+}
