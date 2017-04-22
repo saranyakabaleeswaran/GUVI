6b648b14efedafe4a76e7d358cbe7dd502e8c5ca
saranya k <saranyakabaleeswaran@gmail.com>
2017-04-22 16:05:07 +0530

    Update shapes.cpp

--- shapes.cpp
+++ shapes.cpp
@@ -1,58 +1,39 @@
-#include "iostream"
-#include "conio.h"
+#include<iostream>
 using namespace std;
-class measure
-{
-	public:
-		void shape(int r);
-		void shape(int l,int b);
-		void shape(float t,int d,int e);
-		
-};
-void measure::shape(int r)
-{
-	cout<<"area of the circle is "<<3.14*r*r;
-}
-void measure::shape(int l,int b)
-{
-	cout<<"area of the rectangle is"<<l*b;
-}
-void measure::shape(float t,int d,int e)
-{
-	cout<<"area of the triangle is"<<t*d*e;
-}
-
+int area(int);
+int area(int,int);
+float area(float);
+float area(float,float);
 int main()
 {
-	int r,d,e,l,b;
-	float t;
-  char ch;
-	measure obj;
-	
-	cout<<"\n\n1. area of circle";
-	cout<<"\n2. area of rectangle";
-	cout<<"\n3. area of triangle";
-	
-	cin>>ch;
-	switch(ch)
-	{
-		case 1:
-			cout<<"enter the value of radius of the circle \n";
-			cin>>r;
-			obj.shape(r);
-			break;
-		case 2:
-			cout<<"enter the sides of rectangle \n";
-			cin>>l>>b;
-			obj.shape(l,b);
-			break;
-		case 3:
-			cout<<"enter the sides of triangle \n";
-			cin>>d>>e;
-			obj.shape(0.5,d,e);
-			break;
-		
-		default:
-			cout<<"\nThe choice entered is a wrong choice";
-	}
-	getch();
+	int s,l,b;
+	float r,bs,ht;
+	cout<<"Enter side of a square:";
+	cin>>s;
+	cout<<"Enter length and breadth of rectangle:";
+	cin>>l>>b;
+	cout<<"Enter radius of circle:";
+	cin>>r;
+	cout<<"Enter base and height of triangle:";
+	cin>>bs>>ht;
+	cout<<"Area of square is"<<area(s);
+	cout<<"\nArea of rectangle is "<<area(l,b);
+    cout<<"\nArea of circle is "<<area(r);
+    cout<<"\nArea of triangle is "<<area(bs,ht);
+}
+int area(int s)
+{
+    return(s*s);
+}
+int area(int l,int b)
+{
+    return(l*b);
+}
+float area(float r)
+{
+    return(3.14*r*r);
+}
+float area(float bs,float ht)
+{
+   return((bs*ht)/2);
+}
